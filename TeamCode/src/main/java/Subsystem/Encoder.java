46066df086c71dedc118d3b720b32bc6c1f02762
Shloka Gupta <iamshlokagupta@gmail.com>
2021-09-30 20:01:19 -0500

    Added a SS sorter Class

---
+++ TeamCode/src/main/java/Subsystem/Encoder.java
@@ -0,0 +1,26 @@
+package Subsystem;
+
+public class Encoder {
+
+    //Declaration of class variables
+    //data_type Varriable name = inital_value;
+    static final int MOTOR_TICK_COUNT = 1120;
+    int motorTargetPosition;
+    int distanceToBeMoved;
+
+    //Constructor
+    //function with name same as the class name
+    public void Encoder(){
+        int encoderDrivingTarget = (int)(rotationsNeeded * MOTOR_TICK_COUNT)
+    }//end of constructor
+
+    //Declare functions
+    //scope return_type function_name(input arguments){}
+
+    //function to reset motor
+
+    //function to calculate target position
+
+    //funciton to move motor to targetposition
+
+}//end of class
